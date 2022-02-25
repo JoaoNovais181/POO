673dbe9c9a050c92d7cc9bee1255406a9351ac1c
JoaoNovais181 <73496007+JoaoNovais181@users.noreply.github.com>
2022-02-25 01:47:59 +0000

    Add files via upload

---
+++ POO/Ficha1/Exemplo3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exemplo3
+{
+    /** Calcular o somatório de 10 numeros introduzidos pelo utilizador */
+    public static void somatorio ()
+    {
+        int soma = 0;
+        Scanner sc = new Scanner(System.in);
+
+        for (int i=0 ; i<10 ; i++)
+        {
+            System.out.print("Insira o proximo inteiro: ");
+            soma += sc.nextInt();
+        }
+
+        sc.close();
+        System.out.println("O somatório é: " + soma);
+    }
+    /** Iniciar o programa */
+    public static void main (String[] args)
+    {
+        somatorio();
+    }
+}
---
+++ POO/Ficha1/Exercicio5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercicio5
+{
+
+    public static void main (String[] args)
+    {
+        areatriangulo();
+    }
+
+    public static void areatriangulo ()
+    {
+        Scanner sc = new Scanner(System.in);
+        float base = 1.0f, altura = 1.0f;
+
+        while (base > 0.0f)
+        {
+            System.out.println("Introduza base e altura:");
+            base = sc.nextFloat();
+            altura = sc.nextFloat();
+            if (base<0 || altura<0)
+            {
+                System.out.println("Valores devem ser maiores que zero.");
+                continue;
+            }
+            System.out.printf("A área do triângulo é: %.5f\n", base*altura/2);
+        }
+        sc.close();
+    }
+}
